a929634d1ef19bee0a833d87eebfe8bcdf018fb6
nikolaos karapas <nikos.karapas86@gmail.com>
2021-04-02 08:27:28 +0300

    initial commit

---
+++ src/actions/questions.js
@@ -0,0 +1,8 @@
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+export const ADD_QUESTION = 'ADD_QUESTION';
+export const SAVE_QUESTION_ANSWER = 'SAVE_QUESTION_ANSWER';
+
+
+export function addQuestion(question){
+    return 
+}
